5e8791a1408f066070a2568acc1cba945f2fa6e0
colemanator <peter.coleman143@gmail.com>
2015-10-26 18:05:35 +1000

    Improved Commenting and Readability

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,28 +1,36 @@
 google.load('visualization', '1.1', {packages: ['line']});
 
-
+/*
+ * Draw the graph and also display text and total
+ *
+ * @param The JSO that contains the reasults
+ * @param The min value entered by the user
+ * @param the max value entered by the user
+ */
 function drawChart(graphData,min,max) {
 
+    //variables
     var chunksize = Math.round((max - min)/100);
-
     var data = new google.visualization.DataTable();
+    var total = 0;
+
+    //start preparing data for table
     data.addColumn('number', 'number');
     data.addColumn('number', 'Number of Primes');
 
-    var total = 0;
 
+    //for each key and number in teh JSO put it in the table data object
     for(var key in graphData){
         var number = key;
         var numberOfPrimes = graphData[key];
 
+        //count the number of primes
         total += parseInt(numberOfPrimes);
 
         data.addRow([parseInt(number), parseInt(numberOfPrimes)]);
     }
 
-    document.getElementById('primes-text').innerHTML = 'Prime Numbers between ' + min +' and ' + max;
-    document.getElementById('num-primes').innerHTML = 'Total: ' + total;
-
+    //table options
     var options = {
         chart: {
             title: 'Prime Number distribution Chart',
@@ -33,12 +41,23 @@ function drawChart(graphData,min,max) {
         height: 500
     };
 
+    //draw table
     var chart = new google.charts.Line(document.getElementById('linechart_material'));
-
     chart.draw(data, options);
+
+    //display text and total
+    document.getElementById('primes-text').innerHTML = 'Prime Numbers between ' + min +' and ' + max;
+    document.getElementById('num-primes').innerHTML = 'Total: ' + total;
 }
 
+/*
+ * Function called when the users has finished entering data
+ * makes an ajax call back to the server to find, calculate and display the
+ * graph and total
+ */
 function ajaxGetNumPrimes() {
+
+    //get the user input
     var min = document.getElementById("min").value;
     var max = document.getElementById("max").value;
 
@@ -48,9 +67,12 @@ function ajaxGetNumPrimes() {
     xmlhttp.onreadystatechange = function() {
         //if successful
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
             //get the response text and parse it as JSON
             var primeNumbers = xmlhttp.responseText;
             var primeNumbersJSON = JSON.parse(primeNumbers);
+
+            //Call the draw function 
             drawChart(primeNumbersJSON, min,max);
 
         }
--- routes/index.js
+++ routes/index.js
@@ -11,47 +11,77 @@ module.exports = router;
 
 router.get('/primeNumbers', function(request, res) {
 
+  //variable for the final JSON string
   var graphDataString;
 
+  //Function call with call back
   calculatePrimes(function(){
     res.send(graphDataString)
   });
 
+  /*
+   * This function finds the prime number within the given user range
+   * It Then counts the number of primes per chunk and stores that number in a
+   * Javascript object which is prepared as a string to be set back to the user
+   *
+   * @param A call back
+   */
+  function calculatePrimes(next) {
 
-function calculatePrimes(next) {
+    //get user input
+    var min = parseInt(request.query.min);
+    var max = parseInt(request.query.max);
 
-  var min = parseFloat(request.query.min);
-  var max = parseFloat(request.query.max);
-  var graphData= {};
-  var numPrimes = 0;
-  var chunkSize = Math.round((max-min)/100);
-  var chunk = 0;
-  var count = 0;
-  if(chunkSize < 1){
-    chunkSize = 1;
-  }
-  var prime;
+    //Make sure input is valid
+    if(isNaN(min) || min < 0){
+      min = 0;
+    }
 
-  for (var i = min; i < max; i++) {
-    chunk++;
-    prime = true;
-    for (var n = 2; n <= i - 1; n++) {
+    if(isNaN(max) || max < 0){
+      max = 10000;
+    }
 
-      if (i % n == 0) {
-        prime = false;
+    if(min > max){
+      min = 0;
+      max = 10000;
+    }
+
+    //variables
+    var graphData= {};
+    var numPrimes = 0;
+    var chunkSize = Math.round((max-min)/100);
+    var chunk = 0;
+    var count = 0;
+    var prime;
+
+    //Chunk size can't be less than one
+    if(chunkSize < 1){
+      chunkSize = 1;
+    }
+
+    /* Iterate through and finds prime numbers (change this to use square numbers, more efficient),
+     * Iterates though each chunk and count the number of primes and record the reasults
+     */
+    for (var i = min; i < max; i++) {
+      chunk++;
+      prime = true;
+      for (var n = 2; n <= i - 1; n++) {
+        if (i % n == 0) {
+          prime = false;
+        }
+      }
+      count++;
+      if (prime) {
+        numPrimes++;
+      }
+      if(count == chunkSize){
+        //store number of primes in chunk and reset for next chunk
+        graphData[chunk] = numPrimes;
+        numPrimes = 0;
+        count = 0;
+
       }
     }
-    count++;
-    if (prime) {
-      numPrimes++;
-    }
-    if(count == chunkSize){
-      graphData[chunk] = numPrimes;
-      numPrimes = 0;
-      count = 0;
-
-    }
-  }
 
   graphDataString = JSON.stringify(graphData);
 
